cac3866854d03a4ec71ac485875e384defee1554
Julien Desrosiers <juliend2@macbook-pro-de-julien-desrosiers.local>
2009-08-18 21:12:56 -0400

    continuer

--- spartan.rb
+++ spartan.rb
@@ -16,7 +16,7 @@ EOS
 
 
 
-objects = Parser.new.parse(code)
+p objects = Parser.new.parse(code)
 
 
 def ifarg(objet)
@@ -51,12 +51,13 @@ def classnode(name, body)
   body
 end
 
-def defnode(name, body)
+def defnode(name, params, body)
   # puts "function "
   @f.write('function ')
   yield name
-  # puts "(){"
-  @f.write('(){')
+  @f.write('(')
+  yield params
+  @f.write('){')
   yield body
   # puts "}"
   @f.write('}')
@@ -72,7 +73,6 @@ def literalnode(node)
 end
 
 def node(objet)
-  # puts objet.class
   
   if objet.instance_of?(IfNode)
     ifnode(objet.instance_variable_get(:@condition), objet.instance_variable_get(:@body)) do |txt|
@@ -101,8 +101,8 @@ def node(objet)
   end
   
   if objet.instance_of?(DefNode)
-    defnode(objet.instance_variable_get(:@name), objet.instance_variable_get(:@body)) do |txt|
-      # puts txt
+    defnode(objet.instance_variable_get(:@name), objet.instance_variable_get(:@params), objet.instance_variable_get(:@body)) do |txt|
+      # puts txt.to_s + " : " + txt.class.to_s + " " + txt.length.to_s unless 
       if txt.is_a?(Awesome)
         node(txt)
       elsif(txt.instance_of?(Array))
@@ -139,9 +139,9 @@ def node(objet)
     end
   end
   
-  # if objet.instance_of?(String)
-  #     yield objet if block_given?
-  #   end
+  if objet.instance_of?(String)
+    @f.write('"' + objet + '"')
+  end
 end
 
 
@@ -154,49 +154,24 @@ if objects.instance_of?(Nodes)
 end
 @f.close()
 
-
-# <Nodes @nodes=[
-#   <ClassNode @name="Awesome", @body=<Nodes @nodes=[
-#     <Nodes @nodes=[
-#       <Nodes @nodes=[
-#         <DefNode @name="initialize", @params=["name"], @body=<Nodes @nodes=[
-#           <CallNode @method="pass">
-#         ]>>
-#       ]>,
-#       <DefNode @name="x", @body=<Nodes @nodes=[
-#         <LiteralNode @value=2>
-#       ]>>
-#     ]>
-#   ]>>,
-#   <IfNode @condition=<LiteralNode @value=true>, @body=<Nodes @nodes=[
-#     <SetLocalNode @name="aw",
-#                   @value=<CallNode @method="new",
-#                                    @arguments=[<LiteralNode @value="brilliant!">],
-#                                    @receiver=<GetConstantNode @name="Awesome">>>
-#   ]>, @else_body=<Nodes @nodes=[
-#     <CallNode @method="weird">
-#   ]>>
-# ]>
-
-
-# <Nodes:0x933b4 @nodes=[
-#   <ClassNode:0x93404 @name="Awesome", @body=#<Nodes:0x93468 @nodes=[
-#     <Nodes:0x93620 @nodes=[
-#       <Nodes:0x93684 @nodes=[
-#         <DefNode:0x936d4 @name="initialize", @body=#<Nodes:0x93738 @nodes=[
-#           <CallNode:0x9379c @receiver=nil, @arguments=[], @method="pass">
-#         ]>, @params=["name"]>
-#       ]>, 
-#       <DefNode:0x93508 @name="x", @body=#<Nodes:0x93580 @nodes=[
-#         <LiteralNode:0x935d0 @value=2>
-#       ]>, @params=[]>
-#     ]>
-#   ]>>, 
-#   <IfNode:0x93080 @else_body=#<Nodes:0x930e4 @nodes=[
-#     <CallNode:0x93148 @receiver=nil, @arguments=[], @method="weird">
-#   ]>, @body=#<Nodes:0x931ac @nodes=[
-#     <SetLocalNode:0x931fc @name="aw", @value=#<CallNode:0x9324c @receiver=#<GetConstantNode:0x93314 @name="Awesome">, @arguments=[
-#       <LiteralNode:0x932c4 @value="brilliant!">
-#     ], @method="new">>
-#   ]>, @condition=#<LiteralNode:0x93364 @value=true>>
+#<Nodes:0x8ff34 @nodes=[
+# #<ClassNode:0x8ff84 @body=#<Nodes:0x8ffe8 @nodes=[
+#   #<Nodes:0x901a0 @nodes=[
+#     #<Nodes:0x90204 @nodes=[
+#       #<DefNode:0x90254 @body=#<Nodes:0x902b8 @nodes=[
+#         #<CallNode:0x9031c @arguments=[], @method="pass", @receiver=nil>
+#       ]>, @params=["name"], @name="initialize">
+#     ]>, 
+#     #<DefNode:0x90088 @body=#<Nodes:0x90100 @nodes=[
+#       #<LiteralNode:0x90150 @value=3>
+#     ]>, @params=[], @name="x">
+#   ]>
+# ]>, @name="Awesome">, 
+# #<IfNode:0x8fc00 @body=#<Nodes:0x8fd2c @nodes=[
+#   #<SetLocalNode:0x8fd7c @name="aw", @value=#<CallNode:0x8fdcc @arguments=[
+#     #<LiteralNode:0x8fe44 @value="brilliant!">
+#   ], @method="new", @receiver=#<GetConstantNode:0x8fe94 @name="Awesome">>>
+# ]>, @condition=#<LiteralNode:0x8fee4 @value=true>, @else_body=#<Nodes:0x8fc64 @nodes=[
+#   #<CallNode:0x8fcc8 @arguments=[], @method="weird", @receiver=nil>
+# ]>>
 # ]>
